52fbfcb6e785a1707242647e2089c0229d4e41ea
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2026-03-15 10:05:24 +0900

    useReducer

--- packages/counter-app/src/App.tsx
+++ packages/counter-app/src/App.tsx
@@ -1,22 +1,45 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 const MIN_COUNT = 0;
 const MAX_COUNT = 10;
 
+type Action =
+  | {
+      type: "up";
+    }
+  | {
+      type: "down";
+    }
+  | {
+      type: "reset";
+    };
+
+const reducer = (count: number, action: Action) => {
+  if (action.type === "up") {
+    return count + 1;
+  } else if (action.type === "down") {
+    return count - 1;
+  } else if (action.type === "reset") {
+    return MIN_COUNT;
+  } else {
+    throw new Error("No action type is found.");
+  }
+};
+
 export default function App() {
-  const [count, setCount] = useState(MIN_COUNT);
+  const [count, dispatch] = useReducer(reducer, MIN_COUNT);
 
   return (
     <div css={{ display: "flex", gap: 8 }}>
       <span>{count}</span>
       <div css={{ display: "flex", gap: 4 }}>
-        <button disabled={count === MAX_COUNT} onClick={() => setCount((prev) => prev + 1)}>
+        <button disabled={count === MAX_COUNT} onClick={() => dispatch({ type: "up" })}>
           Up
         </button>
-        <button disabled={count === MIN_COUNT} onClick={() => setCount((prev) => prev - 1)}>
+        <button disabled={count === MIN_COUNT} onClick={() => dispatch({ type: "down" })}>
           Down
         </button>
-        <button onClick={() => setCount(MIN_COUNT)}>Reset</button>
+        <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
       </div>
     </div>
   );
